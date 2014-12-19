8480c50c1476125781a751ef7ba8106925f8bee3
kaplone <dvd.brtrnd@gmail.com>
2014-12-19 16:21:08 +0100

    test update avec dedoublonnage des dates

--- LequelFX/src/main/java/LequelFX/LequelFX/Test_update.java
+++ LequelFX/src/main/java/LequelFX/LequelFX/Test_update.java
@@ -1,7 +1,13 @@
 package LequelFX.LequelFX;
 
 import java.net.UnknownHostException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -14,13 +20,26 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
+import javax.xml.bind.DatatypeConverter;
+
 public class Test_update {
+	
+	static LocalDateTime dateCurrentDoc;
+	static long timeCurrentDoc;
+	static long savedTimeDoc;
+	static ZonedDateTime zdt;
+	
+	static LocalDateTime dateResDoc;
+	static long timeResDoc;
+	static ZonedDateTime zdt_;
 
 	public static void main(String[] args) {
 		
 		connecter();
-		inject();
+		//inject();
 		update();
+		
+		//ajout_root();
 	}
 	
 	static MongoClient mongoclient;
@@ -51,7 +70,13 @@ public class Test_update {
 
 	}
 	
-	@SuppressWarnings("unchecked")
+//	protected static void ajout_root(){
+//		
+//		coll.insert(new BasicDBObject())
+//		
+//	}
+	
+	@SuppressWarnings({ "unchecked", "restriction" })
 	public static void update(){
 		
 		int index = 0;
@@ -81,7 +106,8 @@ public class Test_update {
 //		    coll.update(ob, newDocument3 , true, false);
 //		    
 //		}
-		DBCursor cursor = coll.find(new BasicDBObject("fichier" , true));
+		DBCursor cursor = coll.find();
+		//DBCursor cursor = coll.find(new BasicDBObject("fichier" , true));
 		cursor.addOption(com.mongodb.Bytes.QUERYOPTION_NOTIMEOUT);
 		
 		DBObject currentObject;
@@ -103,46 +129,70 @@ public class Test_update {
 		while(cursor.hasNext()){
 			index ++;
 			currentObject = cursor.next();
-			if (tmp != null  &&  tmp.equals((String)currentObject.get("chemin_pere")) && !(newDocument4 == null || newDocument4.toString().equals("{ }")) ){
-				System.out.println(newDocument4.toString());
-				coll.update(currentObject, newDocument4 , true, false);
-				System.out.println("_");
-			}
-			else{
-				
-	    		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
-				newDocument4 = new BasicDBObject();
-				
-				tmp = (String)currentObject.get("chemin_pere");
-				
-				queryMotif = new BasicDBObject("$text", new BasicDBObject("$search", String.format("\"%s\"", tmp ) ) );
-				obj.add(queryMotif);
-				
-				parents = (ArrayList<String>) currentObject.get("parents");
-				chemin = (String) currentObject.get("chemin_pere");
-				
-				taille  = parents.size() - 1;
-				tailleString  = chemin.length();
-				
-				queryTaille  = new BasicDBObject("parents", new BasicDBObject("$size", taille));
+			
+			System.out.println(currentObject.toString());
+			
+			if (currentObject.get("scan") != null){
 
-				obj.add(queryTaille);
+				dateCurrentDoc = LocalDateTime.ofInstant(((Date)(currentObject).get("scan")).toInstant(), ZoneId.systemDefault());
+				System.out.println(dateCurrentDoc);
+				zdt = dateCurrentDoc.atZone(ZoneId.of("Europe/Paris"));
+				timeCurrentDoc = zdt.toInstant().toEpochMilli() / 1000;
+				System.out.println(timeCurrentDoc);
 				
-				queryFinal = new BasicDBObject("$and", obj);
 				
-				res = coll.find(queryFinal);
-				
-				while (res.hasNext()){
+				if (tmp != null
+				    &&  tmp.equals((String)currentObject.get("chemin_pere"))
+				    && !(newDocument4 == null || newDocument4.toString().equals("{ }"))
+				    &&  Math.abs(timeCurrentDoc - savedTimeDoc) < 3) {
+					System.out.println(newDocument4.toString());
+					coll.update(currentObject, newDocument4 , true, false);
+					System.out.println("_");
+				}
+				else{
 					
-					resCur = res.next();
-
-					if (resCur != null){
-						cheminPere = (String)resCur.get("chemin");
-						if(cheminPere.length() == tailleString){
-							System.out.println(index);
-						    newDocument4.append("$set", new BasicDBObject().append("id_pere", ((ObjectId) resCur.get("_id")).toHexString()));
-						    coll.update(currentObject, newDocument4 , true, false);
-						    break;
+		    		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+					newDocument4 = new BasicDBObject();
+					
+					tmp = (String)currentObject.get("chemin_pere");
+					
+					queryMotif = new BasicDBObject("$text", new BasicDBObject("$search", String.format("\"%s\"", tmp ) ) );
+					obj.add(queryMotif);
+					
+					parents = (ArrayList<String>) currentObject.get("parents");
+					chemin = (String) currentObject.get("chemin_pere");
+					
+					taille  = parents.size() - 1;
+					tailleString  = chemin.length();
+					
+					queryTaille  = new BasicDBObject("parents", new BasicDBObject("$size", taille));
+	
+					obj.add(queryTaille);
+					
+					queryFinal = new BasicDBObject("$and", obj);
+					
+					res = coll.find(queryFinal);
+					
+					while (res.hasNext()){
+						
+						resCur = res.next();
+	
+						if (resCur != null){
+							
+							dateResDoc = LocalDateTime.ofInstant(((Date)(resCur.get("scan"))).toInstant(), ZoneId.systemDefault());
+							System.out.println(dateResDoc);
+							zdt_ = dateResDoc.atZone(ZoneId.of("Europe/Paris"));
+							timeResDoc = zdt_.toInstant().toEpochMilli() / 1000;
+							System.out.println(timeResDoc);
+							
+							cheminPere = (String)resCur.get("chemin");
+							if(cheminPere.length() == tailleString && Math.abs(timeCurrentDoc - timeResDoc) < 10 ){
+								savedTimeDoc = timeResDoc;
+								System.out.println(index);
+							    newDocument4.append("$set", new BasicDBObject().append("id_pere", ((ObjectId) resCur.get("_id")).toHexString()));
+							    coll.update(currentObject, newDocument4 , true, false);
+							    break;
+							}
 						}
 					}
 				}
